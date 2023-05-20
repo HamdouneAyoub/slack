cbec0a5907587b87b0990f6b9a138ac841e9d7e1
hamdouneayoub <ayoubhamdoune.pro@gmail.com>
2023-05-20 18:15:00 +0100

    step2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeNavbarComponent } from './component/home-navbar/home-navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginPageComponent } from './component/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',
+  component: HomeComponent
+ },
+ {path:'home-navbar',
+component:HomeNavbarComponent},
+{path:'login-page',
+component:LoginPageComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+  
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { HomeNavbarComponent } from './component/home-navbar/home-navbar.component';
+import { LoginPageComponent } from './component/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeNavbarComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
